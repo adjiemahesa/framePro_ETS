96d301db71430539caba608a53e8cd560a744968
adjiemahesa <adjiemahesa75@gmail.com>
2022-04-07 01:29:22 +0700

    fix autocomplete register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
                 <div>
                     <label for="name" class="block text-sm font-medium text-gray-700">{{ __('Name') }}</label>
                     <div class="mt-1">
-                        <input id="name" name="name" type="text" required autocomplete="email" autofocus required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm form-control @error('name') is-invalid @enderror">
+                        <input id="name" name="name" type="text" required autocomplete="name" autofocus required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm form-control @error('name') is-invalid @enderror">
                         @error('name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
