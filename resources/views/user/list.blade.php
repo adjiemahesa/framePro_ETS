216021d470086415273bf128726a51d0b0faed7d
billharit <billharit@gmail.com>
2022-04-07 01:48:53 +0700

    add user list

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -4,28 +4,43 @@
 
 @endsection
 @section('content')
-<div style="text-align: center;">
-    <h3>LIST BOOK</h3>
-</div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="card">
-            <div class="row">
-                @foreach ($list_book as $book)
-                <div class="col-md-4">
-                    <img src="img/<?php echo $book->path ?>" alt="..." style="height: 250px; width:250px; margin-top:50px">
-                    <div class="caption">
-                        <h3>{{$book->name}}</h3>
-                        <p>Pengarang : {{$book->pengarang}}</p>
-                        <p>Penerbit : {{$book->penerbit}}</p>
-                        <p><a href="{{url('/book/'.$book->id)}}" class="btn btn-primary" role="button">Open</a></p>
-                    </div>
-                </div>
-                @endforeach
+<div class="relative py-16 bg-white overflow-hidden">
+    <div class="relative">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1>
+          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Book List
+          </span>
+        </h1>
+        <p class="mt-8 text-xl text-gray-500 leading-8">
+        “Libraries were full of ideas – perhaps the most dangerous and powerful of all weapons.” -Sarah J. Maas 
+        </p>
+      </div>
+    </div>
+    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+        @foreach ($list_book as $book)
+        <div class="group relative" onClick="parent.location='{{url('/book/'.$book->id)}}' ">
+            <div class="w-full min-h-80 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 ">
+              <img src="img/<?php echo $book->path ?>"  class="object-center object-cover" >
+            </div>
+            <div class="mt-4 flex justify-between">
+              <div>
+                <h3 class="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    {{$book->name}}
+                  </a>
+                </h3>
+                <p class="mt-1 text-sm text-gray-500">{{$book->pengarang}}</p>
+              </div>
+              <p class="text-sm font-medium text-gray-900">{{$book->penerbit}}</p>
+            </div>
+            <div class="mt-4 w-full text-center p-2 border border-gray-300 hover:border-gray-400">
+                <a href="{{url('/book/'.$book->id)}}" class="btn btn-primary" role="button">Open</a>
             </div>
         </div>
+
+        @endforeach
     </div>
 </div>
-
-
 @endsection
